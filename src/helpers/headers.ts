cac593de9924aecfcf89ff5ea1590f1875b3e14e
DWL716 <1239787435@qq.com>
2020-08-30 10:56:15 +0800

    promise error

---
+++ src/helpers/headers.ts
@@ -0,0 +1,44 @@
+import { isPlainObject } from './util'
+
+function normalizeHeaderName(headers: any, normalizeName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizeName && name.toUpperCase() === normalizeName.toUpperCase()) {
+      headers[normalizeName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+export function processHeaders(headers: any, data: any): any {
+  // 处理 Content-Type 头
+  normalizeHeaderName(headers, 'Content-Type')
+
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8'
+    }
+  }
+  return headers
+}
+
+// 处理 响应返回的 header 头
+export function parseHeaders(headers: string): any {
+  let parsed = Object.create(null)
+  if (!headers) {
+    return parsed
+  }
+  headers.split('\r\n').forEach(line => {
+    let [key, val] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) {
+      return
+    }
+    if (val) {
+      val = val.trim()
+    }
+    parsed[key] = val
+  })
+  return parsed
+}
---
+++ src/xhr.ts
@@ -0,0 +1,82 @@
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
+
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers = {}, responseType, timeout } = config
+
+    const request = new XMLHttpRequest()
+
+    // 判断是否设置了返回值的类型
+    if (responseType) {
+      request.responseType = responseType
+    }
+    // 判断是否设置了超时的时间
+    if (timeout) {
+      request.timeout = timeout
+    }
+
+    request.open(method.toUpperCase(), url, true)
+
+    // 响应成功的回调事件
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      if (request.status === 0) {
+        return
+      }
+
+      const responseHeaders = request.getAllResponseHeaders()
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: parseHeaders(responseHeaders),
+        config,
+        request
+      }
+      handleResponse(response)
+    }
+    // 响应失败的回调函数
+    request.onerror = function handleError() {
+      console.log('网络出错')
+      reject(createError('Network Error', config, null, request))
+    }
+    // 超时回调函数
+    request.ontimeout = function handleTimeout() {
+      console.log('超时了')
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+    }
+    // 在请求体上设置 headers
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+    // 发送请求数据
+    request.send(data)
+
+    // 判断请求是否成功 status 在 200 ～ 300 之间
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
+    }
+  })
+}
