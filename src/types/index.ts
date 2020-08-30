f295400d0fff89e1793d1c4829e66343735ab5e2
DWL716 <1239787435@qq.com>
2020-08-30 23:32:39 +0800

    扩展接口

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,7 +17,7 @@ export type Method =
   | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   method?: Method
   data?: any
   params?: any
@@ -43,4 +43,24 @@ export interface AxiosError extends Error {
   code?: string | null
   request?: any
   response?: AxiosResponse
+}
+
+// 定义 Axios 接口的方法
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+// 定义 AxiosInstance 混合接口 继承 Axios 的方法
+export interface AxiosInstance extends Axios {
+  // 定义 接口的参数类型
+  (config: AxiosRequestConfig): AxiosPromise
 }
